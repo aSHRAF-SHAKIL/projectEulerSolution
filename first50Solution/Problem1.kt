8c76a269f3d27b7605a5e6f3a6986434b92d7704
ASHRAF <109437836+JUSTCLICKIT@users.noreply.github.com>
2024-03-27 05:41:37 +0600

    Multiples of 3 or 5

---
+++ first50Solution/Problem1.kt
@@ -0,0 +1,17 @@
+//kotlin solution 03/27/2024
+
+fun main() {
+   
+    val limit = 1000
+    var sum = 0
+
+    for (i in 1 until limit) {
+        if (i % 3 == 0 || i % 5 == 0) {
+            sum += i
+        }
+    }
+
+    println("The sum of all multiples of 3 and 5 below $limit is: $sum")   
+}
+
+//Answer:  233168
